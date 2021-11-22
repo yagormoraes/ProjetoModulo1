c73192e5229947b7dfda313335f2a33ede36b65e
yagormorares <yago.dmoraes@poli.ufrj.br>
2021-11-22 12:30:14 -0300

    Adição dos textos para cada rota (ainda falta atualizar)

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,4 @@
-
+//funções usadas pelo personagem 1 (Neji)
 function personagem1_1() {
 	var opcao = (prompt("Digite o número correspondente a opção de seu interesse"))
 	if (opcao == 1){
@@ -15,7 +15,7 @@ function personagem1_2() {
 	var tent = 1
 	while(tent<=4){
 		var questao = (prompt("Digite sua resposta com o valor exato"))
-		if(questao == 3){
+		if(questao == 4){
 			window.location.href = "personagem1_3.html"
 			break
 		}else if(tent<3){
@@ -31,9 +31,9 @@ function personagem1_2() {
 
 function personagem1_3() {
 	var opcao = (prompt("Digite o número correspondente a opção de seu interesse"))
-	if (opcao == 1){
+	if (opcao == 2){
 		window.location.href = "victory1.html"
-	}else if(opcao == 2){
+	}else if(opcao == 1){
 		window.location.href = "gameOver1_3.html"
 	}else{
 		alert("Digite somente 1 ou 2!")
@@ -41,12 +41,12 @@ function personagem1_3() {
 	
 }
 
-
+//funções usadas pelo personagem 2 (Tenten)
 function personagem2_1() {
 	var opcao = (prompt("Digite o número correspondente a opção de seu interesse"))
-	if (opcao == 1){
+	if (opcao == 2){
 		window.location.href = "personagem2_2.html"
-	}else if(opcao == 2){
+	}else if(opcao == 1){
 		window.location.href = "gameOver2_1.html"
 	}else{
 		alert("Digite somente 1 ou 2!")
@@ -58,7 +58,7 @@ function personagem2_2() {
 	var tent = 1
 	while(tent<=4){
 		var questao = (prompt("Digite sua resposta com o valor exato"))
-		if(questao == 3){
+		if(questao == 326416){
 			window.location.href = "personagem2_3.html"
 			break
 		}else if(tent<3){
@@ -84,12 +84,12 @@ function personagem2_3() {
 	
 }
 
-
+//funções usadas pelo personagem 3 (Rock Lee)
 function personagem3_1() {
 	var opcao = (prompt("Digite o número correspondente a opção de seu interesse"))
-	if (opcao == 1){
+	if (opcao == 2){
 		window.location.href = "personagem3_2.html"
-	}else if(opcao == 2){
+	}else if(opcao == 1){
 		window.location.href = "gameOver3_1.html"
 	}else{
 		alert("Digite somente 1 ou 2!")
@@ -101,7 +101,7 @@ function personagem3_2() {
 	var tent = 1
 	while(tent<=4){
 		var questao = (prompt("Digite sua resposta com o valor exato"))
-		if(questao == 3){
+		if(questao == 145){
 			window.location.href = "personagem3_3.html"
 			break
 		}else if(tent<3){
@@ -117,9 +117,9 @@ function personagem3_2() {
 
 function personagem3_3() {
 	var opcao = (prompt("Digite o número correspondente a opção de seu interesse"))
-	if (opcao == 1){
+	if (opcao == 2){
 		window.location.href = "victory3.html"
-	}else if(opcao == 2){
+	}else if(opcao == 1){
 		window.location.href = "gameOver3_3.html"
 	}else{
 		alert("Digite somente 1 ou 2!")
